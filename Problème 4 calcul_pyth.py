d1b803225e32ea853d96069ba4b52941bacd9b6a
Natpol50 <103749286+Natpol50@users.noreply.github.com>
2023-04-02 14:48:13 +0200

    Update Problème 4 calcul_pyth.py

--- Problème 4 calcul_pyth.py
+++ Problème 4 calcul_pyth.py
@@ -1,6 +1,6 @@
 class Faudio:
     """
-    Classe correspondant à un fichier audio dans le problème 4 de l'édition 2023 du TFJM
+    Classe correspondant à un fichier audio dans le problème 4 de l'édition 2023 du TFJM²
     
     Pour créer un nouveau fichier, il faut faire comme ceci :
     
@@ -41,38 +41,33 @@ class Faudio:
 7/8 = rouge
 8/8 = rouge
 
-"""
+    """
     def __init__(self,notes):
-        self.res = len(notes)
-        self.file =  {}
-        count = 1
-        for i in range(0, self.res):
-            self.file[f"{count}/{self.res}"] = notes[i]
-            count += 1
+        self.res = len(notes)   # On initialise la résolution à la longueur de la liste de notes
+        self.file =  {}         # On crée un dictionnaire vide qui va contenir le fichier audio
+        count = 1               # On initialise un compteur à 1
+        for i in range(0, self.res):     # Pour chaque note dans la liste de notes
+            self.file[f"{count}/{self.res}"] = notes[i]    # On ajoute la note au dictionnaire avec une clé sous la forme "numéro de note/résolution totale"
+            count += 1      # On incrémente le compteur
     
     def affiche_fichier(self):
-        for k in range(0, self.res):
-            print(f"{k+1}/{self.res} = {self.file[f'{k+1}/{self.res}']}")
+        for k in range(0, self.res):    # Pour chaque note dans le fichier audio
+            print(f"{k+1}/{self.res} = {self.file[f'{k+1}/{self.res}']}")     # On affiche la clé et la valeur du dictionnaire
     
     def affiche_resolution(self):
-        print(self.res)
+        print(self.res)     # On affiche la résolution
     
     def value(self, pos, res):
-        pos = 2*pos-1
-        res_2 = 2*res
-        for i in range(0, self.res):
-            if (i+1)/self.res < pos/res_2:
+        pos = 2*pos-1    # On calcule la position de la note dans le nouveau fichier audio
+        res_2 = 2*res    # On calcule la nouvelle résolution
+        for i in range(0, self.res):     # Pour chaque note dans le fichier audio original
+            if (i+1)/self.res < pos/res_2:   # Si la note est avant la position de la nouvelle note
                 True
-            elif (i+1)/self.res > pos/res_2:
-                return self.file[f"{(i+1)}/{self.res}"]
-            elif (i+1)/self.res == pos/res_2:
-                return 'silence'
+            elif (i+1)/self.res > pos/res_2:    # Si la note est après la position de la nouvelle note
+                return self.file[f"{(i+1)}/{self.res}"]     # On retourne la note correspondante dans le fichier audio original
+            elif (i+1)/self.res == pos/res_2:    # Si la note est à la position de la nouvelle note
+                return 'silence'    # On retourne silence
     
     def changement_de_resolution(self, m):
-        self.new = {}
-        count2 = 1
-        for i in range(0, m):
-            self.new[f"{count2}/{m}"] = self.value(count2, m)
-            count2 += 1
-        self.file = self.new
-        self.res = m
+        self.new = {}     # On crée un nouveau dictionnaire vide
+        count2 = 1        # On initialise un compteur à
